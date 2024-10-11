30fc93ccdef7ca75750fface6309516145af77ca
DanieliusRyliskis <ryliskisdanielius@gmail.com>
2024-10-11 17:53:40 +0300

    Fix modules

--- src/cms_content.js
+++ src/cms_content.js
@@ -1,11 +1,10 @@
-import API_TOKEN from "./api_token.js"
-
 const menuContent = document.getElementById("menuContent")
 const exhibitionName = document.getElementById("exhibitionName")
 const exhibitionDescription = document.getElementById("exhibitionDescription")
 const exhibitionLink = document.getElementById("exhibitionLink")
 const exhibitionPhoto = document.getElementById("exhibitionPhoto")
 
+const API_TOKEN = '8d418ae15d342b0f61066aa76c7ec5f8f6a74767499f8f89108655f35ce699aaccea45b7fbcdbd788e11155e2c031c19be9eb54718f9aa76158d0f8a87eceee113a92a8136c0e6b7edd7469ff1954d7e79786da2b98a770052143d766dee3d45e9e85c7f053b4c27e4e3b9bcbc9fab1a62fe13b0eb9aaaf4802c2fdeb3cbb767';
 const request = async function(endpoint) {
     // Sends Tokens That Authenticate The Request
     const options = {
